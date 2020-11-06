4787f048d861ea07ae784dfc210bd7fde474a32b
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-06 17:33:23 +0100

    Trying to fix top animations

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,10 @@ import $ from "jquery";
 //Main code
 $(document).ready(function () {
   $(window).on("beforeunload", function () {
-    $(window).scrollTop(0);
+    $("body").remove();
+    // $(window).scrollTop(0);
   });
-  $(window).scrollTop(0);
+  // $(window).scrollTop(0);
 
   // $("section.top-hero").on("animationstart webkitAnimationStart oAnimationStart", function () {
   //   $("section.top-hero").fadeOut(3000, function () {
