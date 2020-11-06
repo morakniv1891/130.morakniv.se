34136aaa424c75e16b953f0abf37173d7b63c8cc
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-06 15:34:22 +0100

    Adding JS

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   $(window).on("beforeunload", function () {
     $(window).scrollTop(0);
   });
+  $(window).scrollTop(0);
 
   // $("section.top-hero").on("animationstart webkitAnimationStart oAnimationStart", function () {
   //   $("section.top-hero").fadeOut(3000, function () {
