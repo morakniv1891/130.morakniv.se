f53cd1fc5efca4dc9457aca02ea89ebe1fa596e0
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-06 09:58:46 +0100

    Adding JS

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,15 @@ import $ from "jquery";
 $(document).ready(function () {
   var viewportHeight = $(window).height();
 
-  $("html, body").animate(
-    {
-      scrollTop: viewportHeight * 3,
-      complete: function () {
-        //Hide your button here
+  $("html, body")
+    .animate(
+      {
+        scrollTop: viewportHeight * 2,
+        complete: function () {
+          //Hide your button here
+        },
       },
-    },
-    2000
-  );
+      2000
+    )
+    .delay(1000);
 });
