8f425b12e5dadc1d98e387b3d1add98a8106009b
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-06 10:59:09 +0100

    Adding JS

--- src/js/index.js
+++ src/js/index.js
@@ -7,17 +7,17 @@ $(document).ready(function () {
   });
   var viewportHeight = $(window).height();
 
-  setTimeout(function () {
-    $("html, body").animate(
-      {
-        scrollTop: viewportHeight * 2,
-      },
-      3000,
-      function () {
-        $("section.top-hero").fadeOut(300, function () {
-          $(this).remove();
-        });
-      }
-    );
-  }, 1500);
+  // setTimeout(function () {
+  //   $("html, body").animate(
+  //     {
+  //       scrollTop: viewportHeight * 2,
+  //     },
+  //     3000,
+  //     function () {
+  //       $("section.top-hero").fadeOut(300, function () {
+  //         $(this).remove();
+  //       });
+  //     }
+  //   );
+  // }, 1500);
 });
