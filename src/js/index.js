205e9b6f84e88c4ae0304a6f68e262bfcebdb817
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-06 10:05:57 +0100

    Adding JS

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
   $(this).scrollTop(0);
   var viewportHeight = $(window).height();
 
-  setTimeout(function () {
-    $("html, body").animate(
-      {
-        scrollTop: viewportHeight * 2,
-        complete: function () {
-          //Hide your button here
-        },
-      },
-      2000
-    );
-  }, 1500);
+  // setTimeout(function () {
+  //   $("html, body").animate(
+  //     {
+  //       scrollTop: viewportHeight * 2,
+  //       complete: function () {
+  //         //Hide top image
+  //       },
+  //     },
+  //     2000
+  //   );
+  // }, 1500);
 });
