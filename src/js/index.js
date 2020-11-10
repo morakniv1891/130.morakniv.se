f0f34cfa80ed55f9ea610e4d71676b7f7284d24e
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-10 11:41:35 +0100

    Starting from scratch with hero

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,7 @@ import $ from "jquery";
 
 //Main code
 $(document).ready(function () {
-  $(window).on("beforeunload", function () {
-    // $("main").remove();
-    // $(window).scrollTop(0);
+  $(window).on("scroll", function (e) {
+    console.log(e);
   });
-  // $(window).scrollTop(0);
-
-  // $("section.top-hero").on("animationstart webkitAnimationStart oAnimationStart", function () {
-  //   $("section.top-hero").fadeOut(3000, function () {
-  //     $(this).remove();
-  //   });
-  // });
 });
