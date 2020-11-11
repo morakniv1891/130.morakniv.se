172bf2cc6f293377f45023405a36461f8e76bd91
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-11 07:50:23 +0100

    Make top-hero scrollable

--- src/js/index.js
+++ src/js/index.js
@@ -11,14 +11,5 @@ $(document).ready(function () {
   });
   $(window).on("beforeunload", function () {
     $("main").remove();
-    // $(window).scrollTop(0);
   });
-
-  // $(window).scrollTop(0);
-
-  // $("section.top-hero").on("animationstart webkitAnimationStart oAnimationStart", function () {
-  //   $("section.top-hero").fadeOut(3000, function () {
-  //     $(this).remove();
-  //   });
-  // });
 });
