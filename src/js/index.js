0148a0d98040164344aa74f6425ebd30b987426b
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-11 11:56:20 +0100

    Make top-hero scrollable

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import LocomotiveScroll from "locomotive-scroll";
 //Main code
 $(document).ready(function () {
   const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
+    // el: document.querySelector("[data-scroll-container]"),
     smooth: true,
     smoothMobile: true,
     lerp: 0.1,
