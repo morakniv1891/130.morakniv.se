188b15dabe51369a343cee181e492cd6086de89f
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-12 17:39:43 +0100

    Remove Hero after reaching page 1

--- src/js/index.js
+++ src/js/index.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
       if (!menuSticky) {
         document.getElementById("menu-bar").style.top = "-80px";
       }
+      console.log($(window).height());
       console.log(o);
       // let heroContainer = $(".top-hero__container");
       // $(".top-hero__container .left").css({
