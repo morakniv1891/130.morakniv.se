f96849ac2dd5a6b52dbbc24a072f1780851afa50
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-12 13:39:02 +0100

    Remove Hero after reaching page 1

--- src/js/index.js
+++ src/js/index.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
 
   scroll.on("call", function (t, e, i) {
     if (t === "menuappear" && e === "enter") {
-      $(".scroll-to-explore-text").fadeOut(1500);
+      $(".scroll-to-explore-text").fadeOut(2000);
       // $("#menu-bar").css("display", "flex");
       // $("#menu-bar").css("top", "0");
       // $("#menu-bar").hide();
