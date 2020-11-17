33d259af59672301d9e833ceed44451ba17f351b
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-17 10:43:45 +0100

    Fixing xcroll to explore

--- src/js/index.js
+++ src/js/index.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
   scroll.on("scroll", function (o) {
     let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
     let scrollY = o.scroll.y;
-    $(".page-1[data-scroll-section-inview] .middle img").css("opacity", 1 - scrollY / (vh / 3));
+    $(".page-1[data-scroll-section-inview] .middle img").css("opacity", 1 - scrollY / (vh / 2.5));
     $(".page-1[data-scroll-section-inview] .top-line .left").css("opacity", 1 - scrollY / (vh / 3));
     $(".page-1[data-scroll-section-inview] .bottom-line .left").css("opacity", 1 - scrollY / (vh / 2));
     $(".scroll-to-explore").css("opacity", 1 - scrollY / (vh / 3));
