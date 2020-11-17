a30fcbe248c6817574aed2fc5a7728f5ce09d341
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-11-17 10:22:30 +0100

    Fixing xcroll to explore

--- src/js/index.js
+++ src/js/index.js
@@ -25,7 +25,9 @@ $(document).ready(function () {
 
   scroll.on("scroll", function (o) {
     let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    let scrollY = o.scroll.y;
     console.log(vh);
+    console.log(scrollY);
     console.log(o);
   });
 
