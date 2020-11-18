fb63671e2a778ad7c37e8c0a7182f4e116420779
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-18 13:02:13 +0100

    Starting build of page 2

--- src/js/index.js
+++ src/js/index.js
@@ -45,8 +45,8 @@ $(document).ready(function () {
         $(".page-2__knife-image").css("top", "50%");
       } else {
         $(".page-2__knife-image").css("top", 150 - (scrollY / (vh * 2)) * 100 + "%");
-        console.log(1 / ((scrollY / (vh * 2)) * (scrollY / (vh * 2))));
-        console.log(150 - (scrollY / (vh * 2)) * 100);
+        console.log((scrollY / (vh * 2)) * (scrollY / (vh * 2)) !== 0 ? 1 / ((scrollY / (vh * 2)) * (scrollY / (vh * 2))) : 500);
+        // console.log(150 - (scrollY / (vh * 2)) * 100);
       }
     }
 
