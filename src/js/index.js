e6d75a92caee687abf5fcf32ffd195bac2c12adb
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-19 16:19:46 +0100

    New tactics for animation

--- src/js/index.js
+++ src/js/index.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     //Second page movements
     if ($(".page-2[data-scroll-section-inview]").length && scrollY <= vh) {
       let f = Math.cbrt(scrollY / vh);
-      $(".page-2__knife-image").css("top", 200 - 150 * Math.sqrt(f) + "%");
+      $(".page-2__knife-image").css("top", 200 - 150 * Math.cbrt(f) + "%");
       console.log(Math.sqrt(f));
     }
 
