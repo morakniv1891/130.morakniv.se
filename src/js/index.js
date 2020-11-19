04cd65d26ff6eeacba8683fcbafb061280a905b7
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-19 15:35:13 +0100

    New tactics for animation

--- src/js/index.js
+++ src/js/index.js
@@ -41,10 +41,11 @@ $(document).ready(function () {
     $(".scroll-to-explore").css("opacity", 1 - scrollY / (vh / 3));
 
     //Second page movements
+    console.log(knifeOffset);
+
     if ($(".page-1[data-scroll-section-inview]").length && knifeOffset <= 57.5) {
       knifeOffset += o.speed / 10;
       $(".page-2__knife-image").css("top", 115 - knifeOffset + "%");
-      console.log(knifeOffset);
     } else {
       $(".page-2__knife-image").css("top", "57.5%");
     }
