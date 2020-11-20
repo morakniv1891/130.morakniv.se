dc703925b2deaa24340f1293870f2522f1f3d450
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-20 19:42:37 +0100

    Switching to scrolltrigger

--- src/js/index.js
+++ src/js/index.js
@@ -24,7 +24,8 @@ $(document).ready(function () {
       return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
+    // pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
+    pinType: "fixed",
   });
 
   scroll.stop();
