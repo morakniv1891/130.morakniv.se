c98cf5a5c740f4d970e949ed80fc63b15200c8ad
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-20 10:16:31 +0100

    Switching to scrolltrigger

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,6 @@ $(document).ready(function () {
   const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
-    smoothMobile: true,
-    lerp: 0.1,
-    getDirection: true,
-    getSpeed: true,
-    repeat: true,
-    touchMultiplier: 1.5,
   });
 
   scroll.stop();
@@ -24,62 +18,5 @@ $(document).ready(function () {
 
   $(window).on("beforeunload", function () {
     $("main").remove();
-  });
-
-  scroll.on("scroll", function (o) {
-    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    let scrollY = o.scroll.y;
-
-    //First page movements
-    if ($(".page-1[data-scroll-section-inview]").length) {
-      $(".page-1[data-scroll-section-inview] .middle img").css("opacity", 1 - scrollY / (vh / 2.5));
-      $(".page-1[data-scroll-section-inview] .middle img").css("transform", "scale(0.5)");
-      $(".page-1[data-scroll-section-inview] .top-line .left").css("opacity", 1 - scrollY / (vh / 3));
-      $(".page-1[data-scroll-section-inview] .bottom-line .left").css("opacity", 1 - scrollY / (vh / 2));
-    }
-    $(".scroll-to-explore").css("opacity", 1 - scrollY / (vh / 3));
-
-    //Second page movements
-    if ($(".page-2[data-scroll-section-inview]").length && scrollY <= vh) {
-      let f = scrollY / vh;
-      $(".page-2__knife-image").css("top", 150 - 100 * f + "%");
-      console.log(Math.sqrt(f));
-    }
-
-    //Hiding and showing menu
-    if (menuActive && !menuHide) {
-      $("header").css("top", "0");
-    }
-
-    if (o.direction === "down" && menuHide) {
-      $("header").css("top", "-80px");
-    }
-
-    if (o.direction === "up" && menuActive) {
-      $("header").css("top", "0");
-    }
-
-    // console.log(vh);
-    // console.log(scrollY);
-    console.log(o);
-  });
-
-  scroll.on("call", function (t, e, i) {
-    switch (t) {
-      case "menu-entry":
-        if (e === "exit") {
-          menuActive = true;
-        }
-        break;
-      case "menu-hide":
-        menuHide = true;
-        break;
-
-      default:
-        break;
-    }
-    // console.log(t);
-    // console.log(e);
-    // console.log(i);
   });
 });
