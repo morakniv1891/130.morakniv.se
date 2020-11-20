c80e132c4fac8eaab722ab1888cdb22706115870
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-20 11:00:02 +0100

    Switching to scrolltrigger

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
   let menuActive = false,
     menuHide = false;
   const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container"),
     smooth: true,
   });
 
