b3774f3db4d721d6d90eb899765c7c4ee448bf0a
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-23 12:51:26 +0100

    Something broke, have to restart

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   gsap.registerPlugin(ScrollTrigger);
 
   const scroll = new LocomotiveScroll({
-    el: document.querySelector("body"),
+    el: document.querySelector("#main"),
     smooth: true,
   });
 
@@ -16,7 +16,7 @@ $(document).ready(function () {
   scroll.on("scroll", ScrollTrigger.update);
 
   // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("body", {
+  ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
@@ -24,7 +24,7 @@ $(document).ready(function () {
       return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("body").style.transform ? "transform" : "fixed",
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
   });
 
   scroll.stop();
