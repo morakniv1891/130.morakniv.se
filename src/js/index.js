29b191dd51d7272ab75e712d5572a01f54758124
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-23 18:58:48 +0100

    Something broke, have to restart

--- src/js/index.js
+++ src/js/index.js
@@ -199,6 +199,7 @@ $(document).ready(function () {
       gsap.to("#forest-square-image", {
         width: 6000,
         height: 6000,
+        transformOrigin: "50% 50%",
         scrollTrigger: {
           scroller: "#main",
           trigger: "#forest-square-image",
