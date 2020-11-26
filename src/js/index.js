e9995c3c13317b6302fa4cc3d45cc631b634bf81
morakniv1891 <fredrik.vestin@morakniv.se>
2020-11-26 17:15:27 +0100

    Rebuilding to get transition back to black to work

--- src/js/index.js
+++ src/js/index.js
@@ -144,7 +144,7 @@ $(document).ready(function () {
         scroller: "#main",
         trigger: ".page-3",
         start: "top top",
-        end: "+=200%",
+        end: "+=400%",
         pin: ".page-3",
         pinSpacing: false,
       });
