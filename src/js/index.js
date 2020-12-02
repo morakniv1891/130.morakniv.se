157c063034beaa67cee72e97afed5aa46f247e36
morakniv1891 <fredrik.vestin@morakniv.se>
2020-12-02 08:00:21 +0100

    Testing different easing on square image

--- src/js/index.js
+++ src/js/index.js
@@ -156,7 +156,7 @@ $(document).ready(function () {
         scrollTrigger: {
           scroller: "#main",
           trigger: ".page-3",
-          start: "top top+=10%",
+          start: "top top+=20%",
           end: "+=100%",
           scrub: true,
         },
