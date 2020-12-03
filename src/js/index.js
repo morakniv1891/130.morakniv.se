465d35ffe64b0880509f8a71207a17e8a9796fd0
morakniv1891 <fredrik.vestin@morakniv.se>
2020-12-03 11:00:20 +0100

    Building historical photos page

--- src/js/index.js
+++ src/js/index.js
@@ -262,9 +262,9 @@ $(document).ready(function () {
       });
 
       t3.fromTo(".page-7 .image-1", { y: 300 }, { y: -300 }, 0);
-      t3.fromTo(".page-7 .image-2", { y: 200 }, { y: -200 }, 0);
-      t3.fromTo(".page-7 .image-3", { y: 150 }, { y: -150 }, 0);
-      t3.fromTo(".page-7 .image-4", { y: 250 }, { y: -250 }, 0);
+      t3.fromTo(".page-7 .image-2", { y: 150 }, { y: -150 }, 0);
+      t3.fromTo(".page-7 .image-3", { y: 250 }, { y: -250 }, 0);
+      t3.fromTo(".page-7 .image-4", { y: 400 }, { y: -400 }, 0);
     },
   });
 
