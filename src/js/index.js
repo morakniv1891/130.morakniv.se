e24d11ba07110264822662140e7f0b519dabd946
morakniv1891 <fredrik.vestin@morakniv.se>
2020-12-07 09:43:55 +0100

    Showing menu

--- src/js/index.js
+++ src/js/index.js
@@ -102,6 +102,7 @@ $(document).ready(function () {
       //Start "scrolling" away everything but scroll to explore and fade it out
       gsap.to(".page-1 .content-wrapper", {
         y: "-100vh",
+        scale: 0.5,
         opacity: -1,
         scrollTrigger: {
           scroller: "#main",
