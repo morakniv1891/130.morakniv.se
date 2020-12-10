29f2b78ae4aa163f0eb0ad39ce7a27cec689afbd
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-12-10 12:56:53 +0100

    Adding real texts

--- index.php
+++ index.php
@@ -163,8 +163,8 @@
       <div class="first-content-wrapper">
         <img class="knife-in-hand" src="/assets/images/Mora%202000%20Anniversary%20Edition%20Hand%20Bokeh.jpg" alt="Mora 2000 Held in Hand">
         <div class="text-wrapper">
-          <h2 class="title">Lorem ipsum dolor sit amet</h2>
-          <p class="text">This characteristic knife, engraved with the years of Morakniv 1891 – 2021 is a true collector’s item. The new Mora 2000 (S) Anniversary Edition is a must for the outdoor enthusiast on the next adventure. Made in Sweden, refined from our iconic past.</p>
+          <h2 class="title">A NEW ERA OF MORA 2000 (S)</h2>
+          <p class="text">This characteristic knife, engraved with the years of  Morakniv 1891 – 2021 is a true collector’s item. The new Mora 2000 (S) Anniversary Edition is a must for the outdoor enthusiast on the next adventure. Made in Sweden, refined from our iconic past. </p>
         </div>
       </div>
       <div class="second-content-wrapper">
@@ -187,11 +187,8 @@
       <div class="outer-wrapper">
         <div class="content-wrapper">
           <div class="text-wrapper">
-            <h2 class="title">Lorem ipsum dolor sit amet</h2>
-            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.</p>
+            <h2 class="title">THE LEGEND LIVES ON</h2>
+            <p class="text">The knife was actually, despite its name, launched as early as 1991. Since then, it has become a true classic amongst knife users around the world. When the knife was launched nearly a decade before the great imminent millennium, it was given the futuristic name Mora 2000. 30 years have now passed and the Anniversary Edition is our tribute to the original knife.</p>
           </div>
           <picture>
             <source srcset="assets/images/13949%20-%20Mora%202000%20Anniversary%20Edition%20Comparison_m.png" media="(max-width: 1023px)">
