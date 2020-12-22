090405d9b245e2414e0a3468fa1996a4bd16ad7a
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-12-22 12:02:04 +0100

    History section

--- src/js/index.js
+++ src/js/index.js
@@ -312,7 +312,7 @@ $(document).ready(function () {
         scrollTrigger: {
           scroller: "#main",
           trigger: ".page-7",
-          start: "top bottom",
+          start: "top bottom-=25%",
           end: "center center",
           scrub: true,
         },
