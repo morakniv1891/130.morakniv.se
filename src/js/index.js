54c413356efa9c539ffb5080dc9645ca895cce68
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-12-23 13:15:49 +0100

    History section

--- src/js/index.js
+++ src/js/index.js
@@ -88,7 +88,6 @@ $(document).ready(function () {
       tl.from(".page-2 .intro-text", { opacity: 0, duration: 1 });
       tl.to(".page-2 .intro-text", { opacity: 1, duration: 1 });
       tl.to(".page-2 .intro-text", { opacity: 0, duration: 1 });
-      tl.to(".page-2 .knife-image", { opacity: 1, duration: 1 }, 2);
       tl.to(".usp1", { opacity: 1, duration: 1 });
       tl.to(".usp2", { opacity: 1, duration: 1 });
       tl.to(".usp3", { opacity: 1, duration: 1 });
