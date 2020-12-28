6af2d9ce08b66d96f52bde39cf5de6f6717b6baa
Fredrik Vestin <fredrik.vestin@morakniv.se>
2020-12-28 08:22:38 +0100

    History section

--- src/js/index.js
+++ src/js/index.js
@@ -10,39 +10,39 @@ $(document).ready(function () {
 
   gsap.registerPlugin(ScrollTrigger);
 
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-    smoothMobile: true,
-    touchMultiplier: 3,
-    getDirection: true,
-    lerp: 0.05,
-  });
+  // const scroll = new LocomotiveScroll({
+  //   el: document.querySelector("#main"),
+  //   smooth: true,
+  //   smoothMobile: true,
+  //   touchMultiplier: 3,
+  //   getDirection: true,
+  //   lerp: 0.05,
+  // });
 
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  scroll.on("scroll", (args) => {
-    if (args.direction === "down") {
-      $("header.hideable").css("top", "-80px");
-    } else if (args.direction === "up") {
-      $("header.hideable").css("top", "0");
-    }
+  // // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  // scroll.on("scroll", (args) => {
+  //   if (args.direction === "down") {
+  //     $("header.hideable").css("top", "-80px");
+  //   } else if (args.direction === "up") {
+  //     $("header.hideable").css("top", "0");
+  //   }
 
-    ScrollTrigger.update();
-  });
+  //   ScrollTrigger.update();
+  // });
 
-  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
-  });
+  // // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  // ScrollTrigger.scrollerProxy("#main", {
+  //   scrollTop(value) {
+  //     return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+  //   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  //   getBoundingClientRect() {
+  //     return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+  //   },
+  //   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  //   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
+  // });
 
-  scroll.stop();
+  // scroll.stop();
 
   /*
   /*
@@ -521,9 +521,9 @@ $(document).ready(function () {
     $("main").remove();
   });
 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => scroll.update());
+  // // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  // ScrollTrigger.addEventListener("refresh", () => scroll.update());
 
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
+  // // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  // ScrollTrigger.refresh();
 });
