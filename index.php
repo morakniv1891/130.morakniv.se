4e29ad30f6762f19d3ba0b70304fa5f8a8fe57f3
Fredrik Vestin <fredrik.vestin@morakniv.se>
2021-01-07 13:06:28 +0100

    Countdown Logic

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ Ever since the beginning in 1891 we have been manufacturing high-quality knives 
       <div class="second-content-wrapper">
         <div class="text-wrapper">
           <h2 class="title">THE LEGEND LIVES ON</h2>
-          <p class="text">The knife was actually, despite its name, launched as early as 1991. Since then, it has become a true classic amongst knife users around the world. When the knife was launched nearly a decade before the great imminent millennium, it was given the futuristic name Mora 2000. 30 years have now passed and the Anniversary Edition is our tribute to the original knife.</p>
+          <p class="text"><pre>The knife was actually, despite its name, launched as early as 1991. Since then, it has become a true classic amongst knife users around the world. When the knife was launched nearly a decade before the great imminent millennium, it was given the futuristic name Mora 2000. 30 years have now passed and the Anniversary Edition is our tribute to the original knife.</pre></p>
         </div>
         <picture>
           <source srcset="assets/images/Mora%202000%20Original%20Comparison_m.png" media="(max-width: 1024px)">
