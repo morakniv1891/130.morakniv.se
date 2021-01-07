483c9c8738dc90f9b1b11bbc48b5268d2d379600
Fredrik Vestin <fredrik.vestin@morakniv.se>
2021-01-07 09:39:35 +0100

    Countdown Logic

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
       if(seconds === 0 && minutes === 0 && hours === 0 && days === 0) {
         elements = document.querySelectorAll(".countdown-wrapper");
         for (i = 0; i < elements.length; i++) {
-          elements[i].innerHTML = "";
+          elements[i].innerHTML = '<a href="https://morakniv.se/en/products/" class="buy-now-button">Buy Now!</a>';
         }        
       } else {
         if(seconds < 0){
