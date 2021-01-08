e4f7fa45d61986475cb8a48aa5eb5b3d86c26fea
Fredrik Vestin <fredrik.vestin@morakniv.se>
2021-01-08 13:22:14 +0100

    Countdown Logic

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
   $year = 2021;
   $month = 1;
-  $day = 14;
-  $hour = 14;
-  $min = 30;
+  $day = 8;
+  $hour = 12;
+  $min = 24;
   $sec = 0;
 
   $target = mktime($hour,$min,$sec,$month,$day,$year);
