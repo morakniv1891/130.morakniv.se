2a267508284e4596310e54742fc9694dc6fa466e
Fredrik Vestin <fredrik.vestin@morakniv.se>
2021-01-08 10:27:34 +0100

    Countdown Logic

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
   <script src="assets/js/index.js"></script>
+  <script src="node-modules/cookie-notice/dist/cookie.notice.min.js"></script>
   <script>
     var days = <?php echo $rDay; ?>;
     var hours = <?php echo $rHour; ?>;
