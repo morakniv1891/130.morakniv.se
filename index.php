1a63c6de96e68bc744439f8f3f43a9de2047709d
Fredrik Vestin <fredrik.vestin@morakniv.se>
2021-01-13 11:35:14 +0100

    Adding OG data

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
   <meta property="og:url" content="https://130.morakniv.se">
   <meta property="og:description" content="The year 2021 marks the 130-year Anniversary of Morakniv. Ever since the beginning in 1891 we have been manufacturing high-quality knives in Mora, Sweden, proven to stand the test of time.">
   <meta property="og:type" content="website">
-  <meta property="og:image" content="https://130.morakniv.se/assets/images/Mora%202000%20Anniversary%20Edition%20(S)%20(43)_web.jpg">
+  <meta property="og:image" content="https://130.morakniv.se/assets/images/Mora%202000%20Anniversary%20Edition%20(S)%20Poster.jpg">
 </head>
 
 <body>
