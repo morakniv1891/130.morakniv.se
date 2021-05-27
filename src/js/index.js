918e140b0ea5c98194cd6ca4573b40e7faaf8a0e
morakniv1891 <fredrik.vestin@morakniv.se>
2021-05-27 13:18:13 +0200

    Trying to turn off smotth scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import LocomotiveScroll from "locomotive-scroll";
+// import LocomotiveScroll from "locomotive-scroll";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import "../../assets/js/cookie.notice.js";
@@ -12,14 +12,14 @@ $(document).ready(function () {
 
   gsap.registerPlugin(ScrollTrigger);
 
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-    smoothMobile: true,
-    touchMultiplier: 3,
-    getDirection: true,
-    lerp: 0.05,
-  });
+  // const scroll = new LocomotiveScroll({
+  //   el: document.querySelector("#main"),
+  //   smooth: true,
+  //   smoothMobile: true,
+  //   touchMultiplier: 3,
+  //   getDirection: true,
+  //   lerp: 0.05,
+  // });
 
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   scroll.on("scroll", (args) => {
@@ -33,18 +33,18 @@ $(document).ready(function () {
   });
 
   // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
-  });
+  // ScrollTrigger.scrollerProxy("#main", {
+  //   scrollTop(value) {
+  //     return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+  //   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  //   getBoundingClientRect() {
+  //     return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+  //   },
+  //   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  //   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
+  // });
 
-  scroll.stop();
+  // scroll.stop();
 
   /*
   /*
