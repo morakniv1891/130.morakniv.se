8102a7fa1b8a42c2b492e798e76bea850938fdf5
morakniv1891 <fredrik.vestin@morakniv.se>
2021-05-27 10:37:35 +0200

    Fixing site manifest link

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
   <link rel="apple-touch-icon" sizes="180x180" href="https://130.morakniv.se/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="https://130.morakniv.se/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="https://130.morakniv.se/favicon-16x16.png">
-  <link rel="manifest" href="https://130.morakniv.se/site.webmanifest">
+  <link rel="manifest" href="/site.webmanifest">
   <link rel="mask-icon" href="https:/130.morakniv.se/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
