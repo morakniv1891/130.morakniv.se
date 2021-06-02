fd016b5d2bacb94745a804b65e1dee61483f4db0
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 18:52:54 +0200

    Building start page

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,26 @@ import $ from "jquery";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+$(document).ready(() => {
+  gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.create({
-  trigger: ".first-page",
-  start: "top top",
-  pin: true,
-  pinSpacing: false,
-});
+  ScrollTrigger.create({
+    trigger: ".first-page",
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+  });
 
-ScrollTrigger.create({
-  trigger: ".second-page",
-  start: "top top",
-  pin: true,
-  pinSpacing: false,
-});
+  ScrollTrigger.create({
+    trigger: ".second-page",
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+  });
 
-ScrollTrigger.create({
-  trigger: ".third-page",
-  start: "top top",
-  pin: true,
+  ScrollTrigger.create({
+    trigger: ".third-page",
+    start: "top top",
+    pin: true,
+  });
 });
