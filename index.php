51899871eaa427db84354d88e5792000115eeed4
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 13:57:28 +0200

    Building start page

--- index.php
+++ index.php
@@ -133,6 +133,7 @@
   </header>
   <main id="main">
     <section class="start-page layer">
+      START
       <div class="text-container">
         <div class="header">Morakniv 130-year Anniversary</div>
         <div class="text">Ever since the beginning in 1891 we have been manufacturing high-quality knives in Mora, Sweden, proven to stand the test of time. Fueled by passion and commitment for craftsmanship we take pride in our Swedish heritage and look forward to provide long-lasting products for generations to come.</div>
