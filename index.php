b77517d4f0a30c5d92c56187338db28f81b4e9d4
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 15:56:59 +0200

    Building start page

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-  $year = 2021;
-  $month = 1;
-  $day = 25;
-  $hour = 16;
-  $min = 0;
-  $sec = 0;
-
-  $target = mktime($hour,$min,$sec,$month,$day,$year);
-  $current = time();
-  $difference = $target - $current;
-
-  $rDay = floor($difference/60/60/24);
-  $rHour = floor(($difference-($rDay*60*60*24))/60/60);
-  $rMin = floor(($difference-($rDay*60*60*24)-$rHour*60*60)/60);
-  $rSec = floor(($difference-($rDay*60*60*24)-($rHour*60*60))-($rMin*60));
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,67 +14,7 @@
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
-  <script>
-    var days = <?php echo $rDay; ?>;
-    var hours = <?php echo $rHour; ?>;
-    var minutes = <?php echo $rMin; ?>;
-    var seconds = <?php echo $rSec; ?>;
-    var daysElements,hoursElements,minutesElements,secondsElements,elements;
-    var timeExpired = false;
 
-    function countdown(){
-      seconds--;
-
-      if(seconds < 0){
-        minutes--;
-        seconds = 59;
-      }
-      if(minutes < 0){
-        hours--;
-        minutes = 59;
-      }
-      if(hours < 0){
-        days--;
-        hours = 23;
-      }
-
-      if(seconds < 0 || minutes < 0 || hours < 0 || days < 0) {
-        elements = document.querySelectorAll(".countdown-wrapper");
-        for (i = 0; i < elements.length; i++) {
-          elements[i].innerHTML = '<a href="https://morakniv.se/produkt/mora-2000-s-anniversary-edition/" class="buy-now-button">Product page <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M352 128C352 57.421 294.579 0 224 0 153.42 0 96 57.421 96 128H0v304c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V128h-96zM224 32c52.935 0 96 43.065 96 96H128c0-52.935 43.065-96 96-96zm192 400c0 26.467-21.533 48-48 48H80c-26.467 0-48-21.533-48-48V160h64v48c0 8.837 7.164 16 16 16s16-7.163 16-16v-48h192v48c0 8.837 7.163 16 16 16s16-7.163 16-16v-48h64v272z"/></svg></a>';
-        }
-        timeExpired = true;
-      }
-
-      function pad(n){
-        if (n < 10 && n >= 0){
-          return "0" + n;
-        } else {
-          return n;
-        }
-      }
-
-      elements = document.querySelectorAll(".days-number");
-      for (i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = pad(days);
-      }
-      elements = document.querySelectorAll(".hours-number");
-      for (i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = pad(hours);
-      }
-      elements = document.querySelectorAll(".minutes-number");
-      for (i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = pad(minutes);
-      }
-      elements = document.querySelectorAll(".seconds-number");
-      for (i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = pad(seconds);
-      }
-      if (!timeExpired){setTimeout("countdown()",1000)};
-    }
-
-    document.addEventListener('DOMContentLoaded', countdown);
-  </script>
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-TS9SGFBP9Z"></script>
   <script>
@@ -111,7 +33,7 @@
 </head>
 
 <body>
-  <header id="menu-bar">
+  <!-- <header id="menu-bar">
     <div class="left-link">
       <a href="https://morakniv.se/en" target="_blank">Morakniv.se</a>
       <div class="mobile-menu-toggle">
@@ -130,7 +52,7 @@
     <div class="right-link">
       <a href="https://morakniv.se/produkt/mora-2000-s-anniversary-edition/" target="_blank">Product page</a>
     </div>
-  </header>
+  </header> -->
   <main id="main">
     <section class="start-page layer">
       START
