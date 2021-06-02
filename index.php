7265412a6b19329edda63ebd22c19f767d053092
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 18:50:08 +0200

    Building start page

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="/assets/css/main.min.css">
+  <script src="/assets/js/index.min.js"></script>
+</head>
 
 <body>
   <section class="first-page">ONE</section>
