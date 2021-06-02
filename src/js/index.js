c7b540a500fae5aa66c5b8b551a9418baccdb219
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 12:29:15 +0200

    Trying to get menu to show/hide with scrolltrigger

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,25 @@ import fitVids from "fitvids";
 $(document).ready(function () {
   gsap.registerPlugin(ScrollTrigger);
 
+  //Show/Hide Menu
+  const showMenu = gsap
+    .from("header", {
+      yPercent: -100,
+      paused: true,
+      duration: 0.2,
+    })
+    .progress(1);
+
+  ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+      self.direction === -1 ? showMenu.play() : showMenu.reverse();
+    },
+  });
+
+  //Reset page on unload
   $(window).on("beforeunload", function () {
     $("main").remove();
   });
-
-  fitVids(".page-11");
 });
