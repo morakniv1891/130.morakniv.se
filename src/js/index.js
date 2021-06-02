e41762ae216c418e2471615ace6da3f474cf4934
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-02 15:58:35 +0200

    Building start page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import "../../assets/js/cookie.notice.js";
+// import "../../assets/js/cookie.notice.js";
 import fitVids from "fitvids";
 
 //Main code
