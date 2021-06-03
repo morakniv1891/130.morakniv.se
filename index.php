15f3b0e07fa386d6451e8b46fd680f1bed36c65f
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-03 19:16:31 +0200

    Building start page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.typekit.net/llc0wgb.css">
   <link rel="stylesheet" href="/assets/css/main.min.css">
   <script src="/assets/js/index.min.js"></script>
