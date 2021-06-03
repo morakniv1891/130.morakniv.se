9b449c71f9ec9d60cb79ae81856b45d6dd725ef2
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-03 18:34:07 +0200

    Building start page

--- index.php
+++ index.php
@@ -7,16 +7,38 @@
 </head>
 
 <body>
-  <section class="start-page">
-    <div class="text-container">
-      <h1 class="header">Morakniv<br>130-year<br>Anniversary</h1>
-      <p class="text">Ever since the beginning in 1891 we have been manufacturing high-quality knives in Mora, Sweden, proven to stand the test of time. Fueled by passion and commitment for craftsmanship we take pride in our Swedish heritage and look forward to provide long-lasting products for generations to come.</p>
+    <header id="menu-bar">
+    <div class="left-link">
+      <a href="https://morakniv.se/en" target="_blank">Morakniv.se</a>
+      <div class="mobile-menu-toggle">
+        <input type="checkbox" class="hamburger-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="mobile-menu-wrapper">
+          <a href="https://morakniv.se/en" target="_blank">Morakniv.se</a>
+          <div class="mobile-menu-line"></div>
+          <a href="https://morakniv.se/?post_type=product&p=26977" target="_blank">Product page</a>
+        </div>
+      </div>
     </div>
-  </section>
-  <section class="first-layer">ONE</section>
-  <section class="second-layer">TWO</section>
-  <section class="third-layer">THREE</section>
-  <section class="first-movie">MOVIE</section>
+    <div class="middle-link"><img src="/assets/images/Morakniv130.svg" alt="Morakniv 130 Year Anniversary Logo"></div>
+    <div class="right-link">
+      <a href="https://morakniv.se/produkt/mora-2000-s-anniversary-edition/" target="_blank">Product page</a>
+    </div>
+  </header>
+  <main>
+    <section class="start-page">
+      <div class="text-container">
+        <h1 class="header">Morakniv<br>130-year<br>Anniversary</h1>
+        <p class="text">Ever since the beginning in 1891 we have been manufacturing high-quality knives in Mora, Sweden, proven to stand the test of time. Fueled by passion and commitment for craftsmanship we take pride in our Swedish heritage and look forward to provide long-lasting products for generations to come.</p>
+      </div>
+    </section>
+    <section class="first-layer">ONE</section>
+    <section class="second-layer">TWO</section>
+    <section class="third-layer">THREE</section>
+    <section class="first-movie">MOVIE</section>
+  </main>
 </body>
 
 </html>
