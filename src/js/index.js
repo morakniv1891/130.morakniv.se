30deb85143b1a56fc811300a1417d324ec635aef
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-03 18:31:10 +0200

    Building start page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import "../../assets/js/cookie.notice.js";
 
 $(document).ready(() => {
   gsap.registerPlugin(ScrollTrigger);
