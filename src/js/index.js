c1dd40fe09a0b8ce0a35db666d3187f9e3a3e263
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-18 09:54:50 +0200

    Mora 2000 page styling

--- src/js/index.js
+++ src/js/index.js
@@ -185,7 +185,7 @@ $(document).ready(() => {
         ease: "power1.in",
         scrollTrigger: {
           trigger: ".history",
-          start: "top bottom+=500%",
+          start: "top bottom+=550%",
           end: "top bottom+=125%",
           scrub: true,
         },
