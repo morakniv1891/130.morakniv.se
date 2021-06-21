b7c3ab0b6d9f9e1eb38f651df2fc3ccf6e104965
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-21 13:41:44 +0200

    Copying last pages from old site

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
         </div>
       </div>
       <div class="text-container">
-        <p class="text">Ever since, our knowledge has grown and with infinite passion for craftsmanship we have continued to manufacture knives for knife users all over the world. Through the years Morakniv has made several iconic knives – one of which is the Mora 2000.<br><br>The story of Mora 2000 begins back in 1991 when a Norwegian hunter wished for an all-round knife that could work as a multi-functional tool for the upcoming moose hunt. A knife that could perform in a hunter's environment as well as cutting loaf, spreading butter and slicing sausages during the lunch break.</p>
+        <p class="text">The story of Mora 2000 begins back in 1991 when a Norwegian hunter wished for an all-round knife that could work as a multi-functional tool for the upcoming moose hunt. A knife that could perform in a hunter's environment as well as cutting loaf, spreading butter and slicing sausages during the lunch break.</p>
       </div>
     </section>
     <section class="history-2 boxed">
