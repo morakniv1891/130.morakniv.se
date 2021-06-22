030fab210513902532d2b2cd5e4f9c0a372a6d80
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-22 12:08:54 +0200

    Copying last pages from old site

--- src/js/index.js
+++ src/js/index.js
@@ -206,7 +206,7 @@ $(document).ready(() => {
         ease: "power1.in",
         scrollTrigger: {
           trigger: ".history-1",
-          start: "top bottom+=250%",
+          start: "top bottom+=300%",
           end: "top bottom+=225%",
           scrub: true,
         },
