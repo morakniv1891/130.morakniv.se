c321e801ad89176ac1330beb61404bd586cc3253
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-22 13:12:58 +0200

    Copying last pages from old site

--- src/js/index.js
+++ src/js/index.js
@@ -61,9 +61,9 @@ $(document).ready(() => {
         },
       });
       tl.to(".mora2000-usp .knife-image", { opacity: 0.5, duration: 5 }, 1);
-      tl.from(".mora2000-usp .text,.mora2000-usp .title", { opacity: 0, duration: 1 });
-      tl.to(".mora2000-usp .text,.mora2000-usp .title", { opacity: 1, duration: 3 });
-      tl.to(".mora2000-usp .text,.mora2000-usp .title", { opacity: 0, duration: 1 });
+      tl.from(".mora2000-usp .text, .mora2000-usp .title, .mora2000-usp .product-page-button", { opacity: 0, duration: 1 });
+      tl.to(".mora2000-usp .text, .mora2000-usp .title, .mora2000-usp .product-page-button", { opacity: 1, duration: 3 });
+      tl.to(".mora2000-usp .text, .mora2000-usp .title, .mora2000-usp .product-page-button", { opacity: 0, duration: 1 });
       tl.to(".mora2000-usp .knife-image", { opacity: 1, duration: 1 });
       tl.to(".usp1", { opacity: 1, duration: 1 });
       tl.to(".usp2", { opacity: 1, duration: 1 });
