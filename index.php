553cfa19e197d78862067599b803ecb618cedbd0
morakniv1891 <fredrik.vestin@morakniv.se>
2021-06-22 11:40:12 +0200

    Copying last pages from old site

--- index.php
+++ index.php
@@ -84,6 +84,7 @@
           <img class="knife-image" srcset="assets/images/13949-Mora_2000_Anniversary_Edition.png"
             alt="Mora 2000 Anniversary Edition">
         </picture>
+        <a href="https://morakniv.se/en/product/mora-2000-s-anniversary-edition/" class="product-page-button">Product page <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M352 128C352 57.421 294.579 0 224 0 153.42 0 96 57.421 96 128H0v304c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V128h-96zM224 32c52.935 0 96 43.065 96 96H128c0-52.935 43.065-96 96-96zm192 400c0 26.467-21.533 48-48 48H80c-26.467 0-48-21.533-48-48V160h64v48c0 8.837 7.164 16 16 16s16-7.163 16-16v-48h192v48c0 8.837 7.163 16 16 16s16-7.163 16-16v-48h64v272z"/></svg></a>
         <div class="usp usp1">
           <h3>Friction Grip Handle</h3>
           <div class="line"></div>
@@ -159,8 +160,8 @@
       <h1 class="title intro-text">Mora 2000 (S) Anniversary Edition</h1>
       <div class="image-wrapper">
         <picture>
-          <source srcset="assets/images/13949-Mora_2000_Anniversary_Edition_m.png" media="(max-width: 1024px)">
-          <source srcset="assets/images/13949-Mora_2000_Anniversary_Edition.png" media="(min-width: 1025px)">
+          <source srcset="assets/images/13949-Mora_2000_Anniversary_Edition_Knife_in_Sheath_m.png" media="(max-width: 1024px)">
+          <source srcset="assets/images/13949-Mora_2000_Anniversary_Edition_Knife_in_Sheath.png" media="(min-width: 1025px)">
           <img class="knife-image" srcset="assets/images/13949-Mora_2000_Anniversary_Edition.png"
             alt="Mora 2000 Anniversary Edition">
         </picture>
